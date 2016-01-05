76838d79b98b0e1f8f492f3d0aeb48dc3530dbd4
Bruno Miranda <bruno@bopia.com>
2016-01-05 13:01:12 -0800

    Tweaks

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -3,7 +3,7 @@ class StandupsController < ApplicationController
   def index
     @date = Date.parse(params[:date]) rescue Date.today
     @date_string = @date.strftime("%A")
-    @standups = Standup.where(created_at: @date.at_midnight..@date.next_day.at_midnight).sort_by { rand }
+    @standups = Standup.where(created_at: @date.beginning_of_week.at_midnight..@date.end_of_week.at_midnight)
   end
 
 end
